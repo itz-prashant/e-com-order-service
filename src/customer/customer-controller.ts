ff364367c41207a2d5351543810a4aab9d489bb1
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-10 21:53:23 +0530

    add address endpoint

--- src/customer/customer-controller.ts
+++ src/customer/customer-controller.ts
@@ -1,26 +1,46 @@
-import { Response } from "express"
-import { Request } from "express-jwt"
-import customerModel from "./customer-model"
+import { Response } from "express";
+import { Request } from "express-jwt";
+import customerModel from "./customer-model";
 
 export class CustomerController {
-    getCustomer = async(req:Request, res:Response)=>{
+  getCustomer = async (req: Request, res: Response) => {
+    const { sub: userId, firstName, lastName, email } = req.auth;
 
-        const {sub:userId, firstName, lastName, email } = req.auth
+    const customer = await customerModel.findOne({ userId });
 
-        const customer = await customerModel.findOne({userId})
+    if (!customer) {
+      const newCustomer = await customerModel.create({
+        userId,
+        firstName,
+        lastName,
+        email,
+        address: [],
+      });
 
-        if(!customer){
-            const newCustomer = await customerModel.create({
-                userId,
-                firstName,
-                lastName,
-                email,
-                address:[]
-            })
-
-            return res.json(newCustomer)
-        }
-
-        res.json({customer})
+      return res.json(newCustomer);
     }
+
+    res.json({ customer });
+  };
+
+  addAddress = async (req: Request, res: Response) => {
+    const { sub: userId } = req.auth;
+
+    const customer = await customerModel.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        userId,
+      },
+      {
+        $push: {
+          address: {
+            text: req.body.addresses,
+            isDefault: false,
+          },
+        },
+      },
+      {new: true}
+    );
+      return res.json(customer)
+  };
 }
