61c5395ab555efb37931db7eabb155a5151b27e6
Prashant Gupta <prashantguptaofficial007@gmail.com>
2026-08-16 20:03:37 +0530

    add create Order endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { globalErrorHandler } from "./common/middleware/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import customerRouter from "./customer/customer-router"
 import couponRouter from "./coupon/coupon-router"
+import orderRouter from "./order/order-router"
 
 const app = express();
 app.use(cookieParser());
@@ -14,6 +15,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/customer", customerRouter)
 app.use("/coupons", couponRouter)
+app.use("/orders", orderRouter)
 
 app.use(globalErrorHandler);
 
---
+++ src/order/order-router.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import authenticate from "../common/middleware/authenticate"
+import { asyncWrapper } from "../utils"
+import { OrderController } from "./order-controller"
+
+const router = express.Router()
+
+const orderController = new OrderController()
+
+router.post("/", authenticate, asyncWrapper(orderController.create))
+
+export default router
